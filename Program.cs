96daeb5487efc62a0c144109b35e3e7ddcd08d33
kundan <goswamikundan123@gmail.com>
2023-02-13 17:39:15 +0530

    Adding Program.cs whic  displays Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programming.");
+        }
+    }
+}
