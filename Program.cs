6b649ada798a56bdf26d4e2f37badcb680a7ef6d
kundan <goswamikundan123@gmail.com>
2023-02-13 17:48:25 +0530

    Adding files for FlipCoin problem solution.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Functional Programming.");
+            FlipCoin.calculatepercentage();
         }
     }
 }
